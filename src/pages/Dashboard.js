bc0b222df7dc1947336cd66afb77817cdd12067b
Sunil Kumar D R <sunilrajdr11038@gmail.com>
2026-05-11 16:10:31 +0530

    Removed unnecessary data

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,12 +5,7 @@ const Dashboard = () => {
     <div className='container mt-5'>
       <div className='card shadow p-5 text-center'>
         <h1 className='mb-3'>Sunil's Employee Management System</h1>
-        <h2>Jenkins Auto Build Working</h2>
-        <h2>Jenkins Auto Build Working</h2>
-        <h2>Jenkins Auto Build Working</h2>
-        <h2>Jenkins Auto Build Working</h2>
-        <h2>Jenkins Auto Build Working</h2>
-        <h2>Jenkins Auto Build Working</h2>
+        <h2>Jenkins Auto Build Working automatically</h2>
 
         <p className='lead'>
           Manage employees easily using React JS.
